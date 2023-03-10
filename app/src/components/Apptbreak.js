e8309ef23a263ee7b1ae6c02fd233a7809a93144
OMEGATRYON <tryon.baldwin@student.awesomeinc.org>
2023-03-10 02:31:52 +0000

    Added dinner and appetizer headers for distinguishability

---
+++ app/src/components/Apptbreak.js
@@ -0,0 +1,14 @@
+function Apptbreak() {
+    return (
+        <div id="chi" className="container">
+            <div className="row">
+                <div className="col">APPETIZERS!</div>
+            </div>
+        </div>
+
+    )
+
+
+}
+
+export default Apptbreak;
